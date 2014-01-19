a58ca5c46e49a31bb839864b52312d88708797f2
James Stevenson <jminuse@gmail.com>
2014-01-18 23:40:26 -0500

    Usability and readability improvements

--- api.py
+++ api.py
@@ -1,12 +1,13 @@
-import re, time, random
+import re, time, random, sys
 import requests
 
-bad_strings = ['3some']
-good_strings = []
+def get_string_between(string,a,b):
+	start = string.find(a)
+	string = get_thread.content[start:get_thread.content.find(b, start)]
+	return string
 
 def delete_thread(session, content):
-	form_start = content.find('id="delete_button"')
-	form = content[form_start:content.find('</form>', form_start)]
+	form = get_string_between(content, 'id="delete_button"', '</form>')
 	hidden_values = re.findall('type="hidden" name="([^"]+)" (?:id="[^"]+" )?value="([^"]+)"', form)
 	form_data = {'deletethread':'DELETE'}
 	for h in hidden_values:
@@ -19,29 +20,36 @@ def login(username, password):
 	login_response = session.post('https://www.okcupid.com/login', data=credentials)
 	return session
 
-
-session = login('', '')
-
-time.sleep(1+random.random()*2)
-messages_response = session.get('http://www.okcupid.com/messages')
-
-thread_ids = re.findall('threadid="([^"]+)"', messages_response.content)
-
-for thread in thread_ids:
-	thread_data = {'readmsg': 'true', 'threadid': thread, 'folder': 1}
-	time.sleep(1+random.random()*2)
-	get_thread = session.get('http://www.okcupid.com/messages', params=thread_data)
-	message_start = get_thread.content.find('<div class="message_body">')
-	message = get_thread.content[message_start:get_thread.content.find('</div>', message_start)]
-	message = message.lower()
+def score_message(message_original):
+	bad_strings = ['oriental', 'exotic']
+	good_strings = []
+	message = message_original.lower()
 	score = 0
-	
 	for string in bad_strings:
 		if string in message: score -= 10
 	for string in good_strings:
 		if string in message: score += 10
-	
-	if score < 0:
-		delete_thread(session, get_thread.content)
+	return score
 
+username = 'fill this in'
+password = 'fill this in too'
+if username == 'fill this in':
+	print 'Fill in your username and password!'
+	sys.exit(1)
+session = login(username, password)
+
+time.sleep(1+random.random())
+messages_response = session.get('http://www.okcupid.com/messages')
+thread_ids = re.findall('threadid="([^"]+)"', messages_response.content)
+
+for thread in thread_ids:
+	thread_data = {'readmsg': 'true', 'threadid': thread, 'folder': 1}
+	time.sleep(1+random.random())
+	get_thread = session.get('http://www.okcupid.com/messages', params=thread_data)
+	message = get_string_between(get_thread.content, '<div class="message_body">', '</div>')
+
+	if score_message(message) < 0:
+		print 'Deleting "%s"' % message
+		time.sleep(1+random.random())
+		delete_thread(session, get_thread.content)
 
